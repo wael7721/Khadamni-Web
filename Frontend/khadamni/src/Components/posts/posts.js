64c948b4319e9481a6505f33600c73ac8fee4557
unknown <skander.lahbaiel@medtech.tn>
2022-04-22 11:15:43 +0200

    added feature posts

--- Frontend/khadamni/src/Components/posts/posts.js
+++ Frontend/khadamni/src/Components/posts/posts.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { viewPost } from "../../redux/Slices/post.slice";
 import { useEffect, useState } from "react";
+import ProfileAppBar from "../Dashboard/profileAppBar"
 const Axios = require('axios');
+
 
 export default function Post() {
 
-   
+
     const [data, setData] = useState([])
 
     useEffect(() => {
@@ -20,36 +22,45 @@ export default function Post() {
     const arr = data.map((data, index) => {
         return (
             <>
-            <img width={10} height={"400"} src="https://img-0.journaldunet.com/aA_oQO2y9XDwPXM4qfgFI7_OnUw=/600x/smart/9547dd346990418fa8a7c313a6e687b0/ccmcms-jdn/25881197.jpg" className="card-img-top" alt="..." />
-            <div className="card-body">
-                <h5 className="card-title">{data.field}</h5>
-                <p className="card-text">Description</p>
-            </div>
-            
-            <ul className="list-group list-group-flush">
-                <li className="list-group-item">{data.field}</li>
-                <li className="list-group-item">{data.location}</li>
-                <li className="list-group-item">{data.schedule}</li>
-                <button>Check</button>
-                <button>Delete</button>
-                
-            </ul>
-            <></></>
+                <img width={10} height={"400"} src="https://img-0.journaldunet.com/aA_oQO2y9XDwPXM4qfgFI7_OnUw=/600x/smart/9547dd346990418fa8a7c313a6e687b0/ccmcms-jdn/25881197.jpg" className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{data.field}</h5>
+                    <p className="card-text">Description</p>
+                </div>
+
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">{data.field}</li>
+                    <li className="list-group-item">{data.location}</li>
+                    <li className="list-group-item">{data.schedule}</li>
+                    <button>Check</button>
+                    <button>Delete</button>
+
+                </ul>
+                <></></>
 
         )
     })
 
 
     return (
-        <container>
-        <div className="card" /*style="width: 18rem;"*/>
+       <><ProfileAppBar/>
+        <container style={{
+            "width": "90%",
+            "display": "flex",
+            "align": "center",
+            "alignItems": "center",
+            "justifyContent": "center"
+        }}>
             
+            <div className="card" >
+
                 {arr}
-            <div className="card-body">
-                <a href="#" className="card-link">Card link</a>
-                <a href="#" className="card-link">Another link</a>
+                <div className="card-body">
+                    <a href="#" className="card-link">Card link</a>
+                    <a href="#" className="card-link">Another link</a>
+                </div>
             </div>
-        </div>
         </container>
+        </>
     );
 }
