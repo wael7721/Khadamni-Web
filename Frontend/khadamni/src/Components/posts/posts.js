f4a4092636231eaa1380d6c0fa6361afd54c268a
unknown <skander.lahbaiel@medtech.tn>
2022-04-22 03:05:30 +0200

    new

--- Frontend/khadamni/src/Components/posts/posts.js
+++ Frontend/khadamni/src/Components/posts/posts.js
@@ -5,6 +5,7 @@ const Axios = require('axios');
 
 export default function Post() {
 
+   
     const [data, setData] = useState([])
 
     useEffect(() => {
@@ -18,30 +19,37 @@ export default function Post() {
 
     const arr = data.map((data, index) => {
         return (
-           
+            <>
+            <img width={10} height={"400"} src="https://img-0.journaldunet.com/aA_oQO2y9XDwPXM4qfgFI7_OnUw=/600x/smart/9547dd346990418fa8a7c313a6e687b0/ccmcms-jdn/25881197.jpg" className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">{data.field}</h5>
+                <p className="card-text">Description</p>
+            </div>
+            
             <ul className="list-group list-group-flush">
                 <li className="list-group-item">{data.field}</li>
                 <li className="list-group-item">{data.location}</li>
                 <li className="list-group-item">{data.schedule}</li>
+                <button>Check</button>
+                <button>Delete</button>
+                
             </ul>
+            <></></>
 
         )
     })
 
 
     return (
+        <container>
         <div className="card" /*style="width: 18rem;"*/>
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
+            
                 {arr}
             <div className="card-body">
                 <a href="#" className="card-link">Card link</a>
                 <a href="#" className="card-link">Another link</a>
             </div>
         </div>
-        
+        </container>
     );
 }
